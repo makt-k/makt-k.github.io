9ec1abf7cd65a1f93615e0f7ba0b4171e2db1506
K Mak <karen.mak@stx.oxon.org>
2014-04-09 10:03:36 -0400

    tested js for cross-browsers viewing

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  $("a.nav-link").click(function(e){
-    e.preventDefault();
+  $("a.nav-link").click(function(event){
+    event.preventDefault();
     var clickID = $(this).attr("href");
-    $("body").animate({scrollTop: $(clickID).offset().top}, 2000);
+    $("body").animate({scrollTop: $(clickID).offset().top}, "slow");
     return false;
   });
 });
