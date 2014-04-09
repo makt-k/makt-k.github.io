dbf61affd07b8c85a6bc87480885aa9c6cf5c062
K Mak <karen.mak@stx.oxon.org>
2014-04-09 10:47:18 -0400

    fixed nav-link scrolling for firefox

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  $("a.nav-link").click(function(event){
-    event.preventDefault();
+  $("a.nav-link").click(function(e){
+    e.preventDefault();
     var clickID = $(this).attr("href");
-    $("body").animate({scrollTop: $(clickID).offset().top}, "slow");
+    $("html, body").animate({scrollTop: $(clickID).offset().top}, "slow");
     return false;
   });
 });
